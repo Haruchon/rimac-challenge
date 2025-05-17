db7642ab3182259d827fa267bff48215217866b5
Haruchon <brunodiazg99@gmail.com>
2025-05-17 16:56:13 -0500

    pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/rimac-challenge",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     tailwindcss(),
